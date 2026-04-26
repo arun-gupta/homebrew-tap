0fe297779840504aabba2df29857a004561eee10
Arun Gupta <arun.gupta@gmail.com>
2026-04-25 22:18:28 -0700

    feat: bump agentctl to v0.4.0

--- Formula/agentctl.rb
+++ Formula/agentctl.rb
@@ -1,28 +1,28 @@
 class Agentctl < Formula
   desc "CLI combining git worktrees and coding agents to tackle GitHub issues"
   homepage "https://github.com/arun-gupta/agentctl"
-  version "0.3.0"
+  version "0.4.0"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
       url "https://github.com/arun-gupta/agentctl/releases/download/v#{version}/agentctl-darwin-arm64.tar.gz"
-      sha256 "d8f4580729ded1ebcff054e098a82fb3d6779480ab2279c098205deca3d6b0ed"
+      sha256 "0533313e9e758e220c3f8123efbc86addf561f13e50cceb9ddbe5715d5c243a4"
     end
     on_intel do
       url "https://github.com/arun-gupta/agentctl/releases/download/v#{version}/agentctl-darwin-amd64.tar.gz"
-      sha256 "89a8f63bd31b6234d55f9f31d2dc1e376425e2f9a484ab6cf3aa4637bf964723"
+      sha256 "fd5bcdce3d9f2527432bc45722d10bd2b5992a7a652acac85fefe4b52f5268e4"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/arun-gupta/agentctl/releases/download/v#{version}/agentctl-linux-arm64.tar.gz"
-      sha256 "419700b89a122f552bf01d2d7e34a4fcc4f303b7072fee335be65bc041c4d650"
+      sha256 "22a2c7f71a26fc9477b87f401f20d293dfdea841f4233e6f2378e0d424251594"
     end
     on_intel do
       url "https://github.com/arun-gupta/agentctl/releases/download/v#{version}/agentctl-linux-amd64.tar.gz"
-      sha256 "e0155e6891ba8c300fce0cb622846e4abc8a368c7b450ba059fab10f18f9a7f6"
+      sha256 "7d4fd9d5f1f9ee784a5c941f0448caa2f61d3714f9863b5e5e28b68dcdc0d7ca"
     end
   end
 
